7492cd0ac972fb286e5580a980823e2468bd5e2b
Jason Francis <jason.francis@pythontesting.co.uk>
2016-09-22 14:54:28 +0100

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 import unittest
-
-
-
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -22,18 +21,37 @@ class NewVisitorTest(unittest.TestCase):
         # She notices the page title and header mention to-do lists
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish writing the test!')
+        header_test = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+
+        self.assertEqual(
+                    inputbox.get_attribute('placeholder'),
+                    'enter a to-do item'
+        )
+
         # she types "Buy peacock feathers" into a text box
         # (Ediths hobby is tying fly fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+
 
         # When she hits enter, the page updates and now the page lists
-        #"1: Buy peacock deathers" as an item in a todo list
+        #"1: Buy peacock feathers" as an item in a todo list
+        inputbox.send_keys(keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertTrue(
+                   any(row.text == '1: Buy peacock feathers' for row in rows))
+
+
 
         # There is still a test box inviting her to add another item
         # She enters 'Use Peacock feathers to make fly (Edith is very methodical)'
-
+        self.fail('Finish writing the test!')
         # The page has updated to reflect both items on the list
 
         # Edith wonders wether the site will remember her list. Thens she sees
